69084ab3977e6386128d5e95605b724502d7d091
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-30 01:12:44 +0630

    add more options

--- java/src/regex-cli.java
+++ java/src/regex-cli.java
@@ -10,23 +10,93 @@ class RegexCLI {
         String regex = argv[0];
         String fName = argv[1];
 
-        try {
-            File f = new File(fName);
-            Scanner sc = new Scanner(f);
+        Pattern p = Pattern.compile(regex);
+        Scanner sSc = new Scanner(System.in);
 
-            while (sc.hasNextLine()) {
-                String line = sc.nextLine().trim();
-                Boolean b = Pattern.matches(regex, line);
-                if (b == true) {
-                    System.out.println(line + " [matches]");
-                } else {
-                    System.out.println(line);
+        System.out.print("Do you want to match words or each line in the file? (y or n)...");
+        Boolean matchWords = sSc.nextLine().trim() == "y" ? true : false;
+        
+        Boolean countTotal = false;
+        if (matchWords) {
+            System.out.print("Do you want to count total matches or just an inline indicator is fine? (y or n)...");
+            countTotal = sSc.nextLine().trim() == "y" ? true : false;
+        }
+
+        sSc.close();
+
+        if (matchWords) {
+            if (countTotal) {
+                int outer = 0;
+
+                try {
+                    File f = new File(fName);
+                    Scanner sc = new Scanner(f);
+
+                    while (sc.hasNextLine()) {
+                        int inner = 0;
+                        String line = sc.nextLine().trim();
+                        Matcher m = p.matcher(line);
+
+                        while (m.find()) {
+                            inner++;
+                        }
+                        System.out.println(line + " [" + inner + "]");
+                        outer += inner;
+                    }
+                    sc.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("Total =" + outer);
+            } else {
+                try {
+                    File f = new File(fName);
+                    Scanner sc = new Scanner(f);
+
+                    while (sc.hasNextLine()) {
+                        int count = 0;
+                        String line = sc.nextLine().trim();
+                        Matcher m = p.matcher(line);
+
+                        while (m.find()) {
+                            count++;
+                        }
+                        System.out.println(line + " [" + count + "]");
+
+                    }
+                    sc.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
                 }
             }
-            sc.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        } else {
+            int count = 0;
+
+            try {
+                File f = new File(fName);
+                Scanner sc = new Scanner(f);
+
+                while (sc.hasNextLine()) {
+                    String line = sc.nextLine().trim();
+                    Matcher m = p.matcher(line);
+                    Boolean b = m.matches();
+
+                    if (b) {
+                        System.out.println(line + " [matches]");
+                        count++;
+                    } else {
+                        System.out.println(line);
+                    }
+                }
+                sc.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Total = " + count);
         }
+
 
     }
 
